b8b2ca7dd561d119480f71775eb81fb86d2258c4
B O L T <69644334+Adityeahhh@users.noreply.github.com>
2022-04-06 15:39:56 +0530

    re added javascript

---
+++ js/Optimization.js
@@ -0,0 +1,9 @@
+let closeit = document.getElementById('closeSet')
+let links = document.getElementById('links')
+let contain = document.getElementById('herotext')
+
+closeit.addEventListener('click', function () {
+        contain.classList.remove('animate-fadein')
+})
+
+
