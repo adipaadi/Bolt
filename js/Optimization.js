28cfaec0d7d07f03139265fe1695da4a80e1fce2
B O L T <69644334+Adityeahhh@users.noreply.github.com>
2022-04-30 01:33:16 +0530

    Update Optimization.js

--- js/Optimization.js
+++ js/Optimization.js
@@ -1,9 +1,25 @@
 let closeit = document.getElementById('closeSet')
 let links = document.getElementById('links')
+let linksbout = document.getElementById('linksbout')
 let contain = document.getElementById('herotext')
+let skillLink = document.getElementById('skillLink')
+let navbout = document.getElementById('skillham')
+
 
 closeit.addEventListener('click', function () {
         contain.classList.remove('animate-fadein')
 })
+
+
+function closeSkillNav() {
+        if (skillLink.style.display === "block") {
+          skillLink.style.display = "none";
+        } else {
+          skillLink.style.display = "block";
+        }
+}
+      
+
+
 
 
